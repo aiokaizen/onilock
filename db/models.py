17d76224097c5f788e3335ffb3e438e0c1481365
Mouad Kommir <mouadkommir@gmail.com>
2025-02-17 12:51:21 +0100

    First MVP

--- db/models.py
+++ db/models.py
@@ -11,9 +11,20 @@ class Password(BaseModel):
 class Account(BaseModel):
     name: str
     master_password: str = Field(description="Hashed Master Password")
-    passwords: List[Password] = Field(default=list)
+    passwords: List[Password]
 
     def get_password(self, id: str):
         for password in self.passwords:
             if password.id == id:
                 return password
+        return None
+
+    def remove_password(self, id: str):
+        for index, password in enumerate(self.passwords):
+            if password.id == id:
+                del self.passwords[index]
+                break
+
+    def to_dict(self):
+        account = self.model_dump()
+        print("Account:", account)
