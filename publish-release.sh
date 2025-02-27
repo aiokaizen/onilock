dbf39086543d7aedaa8dc5f73e89712ae48c6031
Mouad Kommir <mouadkommir@gmail.com>
2025-02-27 22:05:02 +0000

    Add publish-release script

---
+++ publish-release.sh
@@ -0,0 +1,20 @@
+
+# Get version
+RELEASE=$(grep '^version' pyproject.toml | sed -E 's/version\s*=\s*"([^"]+)"/\1/')
+echo "Generating a release for version 'v$RELEASE'"
+
+# Creating a release tag
+git tag "v$RELEASE"
+git push origin "v$RELEASE"
+
+# Create release
+gh release create "v$RELEASE" --title "Version v$RELEASE" --notes "Automatically Generated."
+
+ # Generate distribution files
+python -m build
+
+# Upload dist files to release.
+gh release upload "v$RELEASE" dist/*
+
+# Remove dist directory.
+rm -rf dist/
