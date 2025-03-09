a5747d54a14bb871f0fd4f052d6c4d28d5bf9000
Mouad Kommir <mouadkommir@gmail.com>
2025-03-09 13:39:44 +0000

    Fix circular import problem v2 add env.py

--- onilock/core/logging_manager.py
+++ onilock/core/logging_manager.py
@@ -13,8 +13,7 @@ from onilock.core.constants import DEBUG_ENV_NAME, TRUTHFUL_STR
 # @TODO: Change logs directory to /var/log/onilock
 LOGS_DIR = Path("/tmp/logs/onilock")
 
-# DEBUG = os.environ.get(DEBUG_ENV_NAME, "false") in TRUTHFUL_STR
-DEBUG = True
+DEBUG = os.environ.get(DEBUG_ENV_NAME, "false").lower() in TRUTHFUL_STR
 
 
 class LoggingManager:
